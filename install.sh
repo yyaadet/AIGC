75e283437c68a1c2b03df3761ffb963655bbcbfa
gongda <pengxiaotao@pengxiataodeMBP.lan>
2023-04-12 18:32:20 +0800

    fix models download error

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pip install --upgrade diffusers[torch] transformers accelerate scipy safetensors
+pip install --upgrade diffusers transformers accelerate scipy safetensors
 pip install -r requirements.txt
-python manage.py download_models
+cd stable_diffusion_webui; python manage.py download_models
